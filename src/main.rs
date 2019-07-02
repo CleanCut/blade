debc8b0d367b8e530ef55fe56ccb6b09c8d15aae
Nathan Stocks <cleancut@github.com>
2019-07-01 22:49:06 -0600

    Section 2: Variables (section complete)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     println!("Welcome to Blade, the best Rusty Sword Arena Client, ever.");
+    let name = "Zork";
+    let host = "localhost";
+    println!("Connecting {} to server {}", name, host);
 }
 
