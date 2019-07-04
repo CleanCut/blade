a7573b014542f335c7e50ee3f99b7c0ff0274f19
Nathan Stocks <cleancut@github.com>
2019-07-03 19:40:43 -0600

    Section 6: Signposts

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,25 @@ use std::sync::mpsc;
 use std::thread;
 
 fn main() {
+    // Welcome & argument parsing
     println!("Welcome to Blade, the best Rusty Sword Arena Client, ever.");
     let name = "Zork";
     let host = "localhost";
     println!("Connecting {} to server {}", name, host);
 
+    // Network System
+
+    // Audio System (separate thread)
     let (tx, rx) = mpsc::channel();
     let handle = thread::spawn(move || {
         audio::audio_loop(rx);
     });
     tx.send("startup").unwrap();
 
+    // Everything else we need
     let mut window = Window::new(None, "Blade of Rustiness");
+
+    // Game
     'gameloop: loop {
         for event in window.poll_game_events() {
             if let GameEvent::Quit = event {
@@ -24,6 +31,7 @@ fn main() {
         }
     }
 
+    // Cleanup
     tx.send("stop").unwrap();
     handle.join().unwrap();
 }
